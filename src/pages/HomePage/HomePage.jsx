052130856d3f3b149a8c5769b55e8f13de92f07a
krava <kravalex2004@gmail.com>
2024-03-16 17:59:01 +0200

    react-hw-05 1.3

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -29,7 +29,7 @@ export default function HomePage() {
   
   return (
     <>
-      <p className={css.top}>Top movies of the day</p>
+      <p className={css.top}>Top movies of the <span>day</span></p>
       <DNA
         visible={loaderActive}
         wrapperStyle={{
