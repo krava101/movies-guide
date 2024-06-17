e4557ce4bda21789c7744ff1d639aa1282508b89
krava <kravalex2004@gmail.com>
2024-06-17 23:32:09 +0300

    Update Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,41 +1,49 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { changeFilter } from '../../redux/filter/slice';
-import { selectFilter } from '../../redux/filter/selectors';
+import { selectFilter, selectSearch } from '../../redux/filter/selectors';
 import { mainFilter } from '../../redux/filter/constants';
-import css from './Filter.module.css';
+import css from './Filter.module.scss';
+import clsx from 'clsx';
 
-export default function Filter() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const filterRef = useRef()
+function Filter() {
+  const [_, setSearchParams] = useSearchParams();
   const dispatch = useDispatch();
-  const selectedFilter = useSelector(selectFilter);
-  const filter = searchParams.get('filter') ? searchParams.get('filter') : selectedFilter;
-  const search = searchParams.get('query') ? searchParams.get('query') : null;
+  const filter = useSelector(selectFilter);
+  const search = useSelector(selectSearch);
 
-  useEffect(() => {
-    Array.from(filterRef.current.children).filter(e => e.dataset.filter === filter ? e.classList.add(css.active) : e.classList.remove(css.active))
-    search && Array.from(filterRef.current.children).forEach(e => e.classList.remove(css.active));
-  }, [filter, search])
-
-  const handleFilter = (event) => {
-    if (event.target !== event.currentTarget) {
-      Array.from(event.currentTarget.children).forEach(e => e.classList.remove(css.active))
-      const newFilter = event.target.dataset.filter;
-      dispatch(changeFilter(newFilter))
-      setSearchParams({
-        filter: newFilter,
-        page: 1,
-      })
-      event.target.classList.add(css.active);
+  function switchActive(date) {
+    if (!search) {
+      return filter === date ? css.active : '';
     }
   }
 
+  const filterDay = clsx(css.filterLink, switchActive(mainFilter.day));
+
+  const filterWeek = clsx(css.filterLink, switchActive(mainFilter.week));
+
+  const handleFilter = event => {
+    if (event.target !== event.currentTarget) {
+      const newFilter = event.target.dataset.filter;
+      dispatch(changeFilter(newFilter));
+      setSearchParams({
+        filter: newFilter,
+        page: 1,
+      });
+    }
+  };
+
   return (
-    <p className={css.top} ref={filterRef} onClick={handleFilter}>Top of the
-      <button className={css.filterLink} data-filter={mainFilter.day}>day</button>
-      <button className={css.filterLink} data-filter={mainFilter.week}>week</button>
+    <p className={css.top} onClick={handleFilter}>
+      Top of the
+      <button className={filterDay} data-filter={mainFilter.day}>
+        day
+      </button>
+      <button className={filterWeek} data-filter={mainFilter.week}>
+        week
+      </button>
     </p>
-  )
+  );
 }
+
+export default Filter;
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,35 +1,52 @@
-import { fetchMovies, fetchNowPlayingMovies, fetchPopularMovies, fetchQueryMovies, fetchTopRatedMovies, fetchUpcomingMovies } from "../../redux/movies/operations";
-import { changePage, changeFilter, setTotalPages } from "../../redux/filter/slice";
-import { selectFilter, selectPage, selectSearch } from "../../redux/filter/selectors";
-import { selectIsLoading, selectTotalPages } from "../../redux/movies/selectors";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { clearMovie } from "../../redux/currentMovie/slice";
-import { mainFilter } from "../../redux/filter/constants";
-import { useEffect } from "react";
-import { clearShow } from "../../redux/currentShow/slice";
+import {
+  fetchMovies,
+  fetchNowPlayingMovies,
+  fetchPopularMovies,
+  fetchQueryMovies,
+  fetchTopRatedMovies,
+  fetchUpcomingMovies,
+} from '../../redux/movies/operations';
+import {
+  changePage,
+  changeFilter,
+  setTotalPages,
+  setSearch,
+} from '../../redux/filter/slice';
+import {
+  selectFilter,
+  selectPage,
+  selectSearch,
+} from '../../redux/filter/selectors';
+import {
+  selectIsLoading,
+  selectTotalPages,
+} from '../../redux/movies/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { clearMovie } from '../../redux/currentMovie/slice';
+import { mainFilter } from '../../redux/filter/constants';
+import { useEffect } from 'react';
+import { clearShow } from '../../redux/currentShow/slice';
 import { Toaster } from 'react-hot-toast';
-import PagePagination from "../../components/PagePagination/PagePagination";
-import MoviesList from "../../components/Movie/MovieList/MovieList";
-import MainFilter from "../../components/MainFilter/MainFilter";
+import PagePagination from '../../components/PagePagination/PagePagination';
+import MoviesList from '../../components/Movie/MovieList/MovieList';
+import MainFilter from '../../components/MainFilter/MainFilter';
 import css from './MoviesPage.module.css';
-
 
 export default function MoviesPage() {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const [searchParams, setSearchParams] = useSearchParams();
-  const selectedPage = useSelector(selectPage);
-
   const navigate = useNavigate();
 
   const totalPages = useSelector(selectTotalPages);
-  const selectedFilter = useSelector(selectFilter);
-  const selectedSearch = useSelector(selectSearch);
+  const page = useSelector(selectPage);
+  const filter = useSelector(selectFilter);
+  const search = useSelector(selectSearch);
 
-  const search = searchParams.get("query") ? searchParams.get("query") : selectedSearch;
-  const page = searchParams.get("page") ? searchParams.get("page") : selectedPage;
-  const filter = searchParams.get("filter") ? searchParams.get("filter") : selectedFilter;
+  const urlSearch = searchParams.get('query');
+  const urlPage = searchParams.get('page');
+  const urlFilter = searchParams.get('filter');
 
   function filtering(filter) {
     switch (filter) {
@@ -56,23 +73,20 @@ export default function MoviesPage() {
         dispatch(changeFilter(mainFilter.day));
     }
   }
-  
+
   useEffect(() => {
-    !search && setSearchParams({ filter: filter, page: page });
     dispatch(clearMovie());
     dispatch(clearShow());
-    dispatch(changeFilter(filter));
-    dispatch(changePage(page));
-    dispatch(setTotalPages(totalPages));
+    urlFilter && dispatch(changeFilter(urlFilter));
+    urlSearch && dispatch(setSearch(urlSearch));
+    urlPage && dispatch(changePage(urlPage));
     search ? dispatch(fetchQueryMovies({ search, page })) : filtering(filter);
-  }, [dispatch, search, filter, page, setSearchParams, totalPages]);
+  }, [dispatch, search, filter, page, urlSearch, urlFilter, urlPage]);
 
-
-  
   return (
     <div className={css.moviesPage}>
       <p className={css.watch}>Discover new movies</p>
-      <MainFilter/>
+      <MainFilter />
       <div className={css.movieListWrapper}>
         {isLoading && <p className={css.loading}>Loading...</p>}
         <MoviesList />
@@ -80,5 +94,5 @@ export default function MoviesPage() {
       {totalPages > 1 && <PagePagination />}
       <Toaster position="top-right" reverseOrder={false} />
     </div>
-  )
+  );
 }
